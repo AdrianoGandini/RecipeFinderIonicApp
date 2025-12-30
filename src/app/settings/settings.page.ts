1848c45008a588c6e4c18badc4ddb3576b364703
unknown <adrianogandini30@gmail.com>
2025-12-30 12:28:10 +0000

    feat(settings): add MyData instance, unit selection radio group, and unit storage logic

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonRadioGroup, IonRadio, IonLabel, IonItem, IonText } from '@ionic/angular/standalone';
+import { MyData } from '../services/my-data';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonRadioGroup, IonRadio,IonLabel, IonItem, IonText]
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  unit:string = "metric";
+
+  constructor(private s: MyData) { }
 
   ngOnInit() {
   }
+
+  onUnitChange() {
+    this.s.set("unit", this.unit); //Debug
+    console.log('Selected unit:', this.unit);
+}
+
 
 }
